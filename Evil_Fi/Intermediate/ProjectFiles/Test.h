49a837086eb6b5b307e6323ef9a8906792fa27b3
hhm89 <hhmida1@naver.com>
2019-09-01 16:27:58 +0900

    테스트 헤더

---
+++ Evil_Fi/Intermediate/ProjectFiles/Test.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "Test.generated.h"
+
+/**
+ *
+ */
+UCLASS()
+class EVIL_FI_API ATestActor : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	FVector m_vDir;
+};
