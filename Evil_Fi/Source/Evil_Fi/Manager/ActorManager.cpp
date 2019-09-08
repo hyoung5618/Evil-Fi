1fe033024dc1858a726c7b6861cc868945c2ac7a
박형우 <hyoung5618@naver.com>
2019-09-08 16:44:04 +0900

    ActorManager 추가

---
+++ Evil_Fi/Source/Evil_Fi/Manager/ActorManager.cpp
@@ -0,0 +1,2 @@
+#include "ActorManager.h"
+
--- Evil_Fi/Source/Evil_Fi/Manager/ActorManager.h
+++ Evil_Fi/Source/Evil_Fi/Manager/ActorManager.h
@@ -1,1 +1,55 @@
-#pragma once
+#include "DesignPattern/Singleton.h"
+#include "Common/GameType.h"
+#include <GameFramework/Actor.h>
+#include <Engine/World.h>
+
+class ActorManager : public TSingleton<ActorManager>
+{
+public:
+
+	template<typename T>
+	bool CreateActor(UWorld* pWorld, FName strName, const FVector& Location, const FRotator& Rotation)
+	{
+		if (pWorld == nullptr)
+			return false;
+
+		FActorSpawnParameters SPAWNPARAMETER;
+		SPAWNPARAMETER.Name = strName;
+		AActor* pActor = pWorld->SpawnActor<T>(Location, Rotation, SPAWNPARAMETER);
+
+		if (pActor) {
+			TArray<TSharedPtr<AActor>>* pArrActor = m_mapActorArry.Find(strName);
+			if (pArrActor == nullptr)
+			{
+				TArray<TSharedPtr<AActor>> arrActor;
+				arrActor.Add(MakeShareable(pActor));
+				m_mapActorArry.Emplace(strName, arrActor);
+			}
+			else
+			{
+				pArrActor->Add(MakeShareable(pActor));
+			}
+			return true;
+		}
+
+		return false;
+	}
+
+	void DeleteActor()
+	{
+
+	}
+
+	void FindActor()
+	{
+
+	}
+
+	void LoopActor()
+	{
+
+	}
+
+private:
+	TMap<FName, TArray<TSharedPtr<AActor>>> m_mapActorArry;
+};
