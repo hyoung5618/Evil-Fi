0ac7da7d2318d727fbda14eb544a94bc7527873a
박형우 <hyoung5618@naver.com>
2019-09-29 17:57:01 +0900

    몬스터 추가 작업중

---
+++ Evil_Fi/Source/Evil_Fi/Contents/Character/Monster.cpp
@@ -0,0 +1,59 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Monster.h"
+#include "Components/InputComponent.h"
+#include "Components/CapsuleComponent.h"
+
+// Sets default values
+AMonster::AMonster()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AMonster::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AMonster::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+void AMonster::NotifyHit(UPrimitiveComponent* MyComp, AActor* Other, UPrimitiveComponent* OtherComp, bool bSelfMoved, FVector HitLocation, FVector HitNormal, FVector NormalImpulse, const FHitResult& Hit)
+{
+	Super::NotifyHit(MyComp, Other, OtherComp, bSelfMoved, HitLocation, HitNormal, NormalImpulse, Hit);
+
+	FDamageEvent DamageEvent;
+	TakeDamage(1.f, DamageEvent, Other->GetInstigatorController(), Other);
+}
+
+void AMonster::NotifyActorBeginOverlap(AActor* OtherActor)
+{
+	Super::NotifyActorBeginOverlap(OtherActor);
+}
+
+void AMonster::NotifyActorEndOverlap(AActor* OtherActor)
+{
+	Super::NotifyActorEndOverlap(OtherActor);
+}
+
+float AMonster::TakeDamage(float Damage, FDamageEvent const& DamageEvent, AController* EventInstigator, AActor* DamageCauser)
+{
+	float fResultDamage = Super::TakeDamage(Damage, DamageEvent, EventInstigator, DamageCauser);
+
+	if (HealthPoint > 0)
+		HealthPoint -= fResultDamage;
+	
+	if (HealthPoint <= 0)
+		Destroy();
+
+	return fResultDamage;
+}
+
